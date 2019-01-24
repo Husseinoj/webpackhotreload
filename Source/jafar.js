bd2579f8ccaeaa3e70e671cb0fe0ae08924aba24
Hussein Ojaghi <HusseinOjaghi1991@gmail.com>
2019-01-24 15:22:10 +0330

    process css file in web

---
+++ Source/jafar.js
@@ -0,0 +1,1 @@
+console.log('jafar object');
--- webpack.config.js
+++ webpack.config.js
@@ -1,108 +1,33 @@
-const enviroment = process.env.NODE_ENV || 'development';
-const autoprefixer = require('autoprefixer');
-
-const webpack = require('webpack');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-
+const mode = process.env.NODE_ENV || 'development';
+const MiniCssExtractPlugin=require('mini-css-extract-plugin');
 const entry = {
-    'main': ['./Source/main.site.ts', './Source/main.site.scss']
+    main:['./Source/index.js','./Source/jafar.js','./Source/gholi.js','./Source/main.css']
 }
-
 const _module = {
-    rules: [{
-            test: /\.tsx?$/,
-            exclude: /node_modules/,
-            use: [
-                'ts-loader'
-            ]
-        },
+    rules:[
         {
-            test: /\.pug$/,
-            exclude: /node_modules/,
-            use: [
-                'raw-loader',
-                'pug-html-loader'
+            test:/\.css$/,
+            exclude:/node_modules/,
+            use:[
+                MiniCssExtractPlugin.loader,
+                'css-loader'
             ]
-        },
-        {
-            test: /\.component.scss$/,
-            exclude: /node_modules/,
-            use: [
-                'css-loader',
-                'postcss-loader',
-                'sass-loader',
-            ]
-          
-        },
-        {
-            test: /\.site.scss$/,
-            exclude: /node_modules/,
-            use: ExtractTextPlugin.extract({
-                fallback: 'style-loader',
-                use: [
-                    'css-loader',
-                    'postcss-loader',
-                    'sass-loader',
-                ]
-            })
         }
     ]
 }
-const optimization = {
-    splitChunks: {
-        cacheGroups: {
-            commons: {
-                test: /[\\/]node_modules[\\/]/,
-                name: 'common',
-                chunks: 'all'
-            }
-        }
-    }
-}
-
 const output = {
     filename: '[name].bundle.js',
-    path: __dirname + '/wwwroot/js/',
-    pathinfo: true
+    path:__dirname+'/wwwroot/js'
 }
-
-if (enviroment === 'production') {
-    output.filename = '[name].bundle.min.js',
-    output.pathinfo = false;
-} else if (enviroment === 'development') {
-    output.publicPath = '/js/';
-}
-
 const plugins = [
-    new ExtractTextPlugin({
-        filename: enviroment === 'production' ? '../css/[name].bundle.min.css' : '../css/[name].bundle.css',
-        disable: false,
-        allChunks: false
-    }),
-    new webpack.LoaderOptionsPlugin({
-        options: {
-            postcss: [
-                autoprefixer()
-            ]
-        }
-    }),
-    new webpack.DefinePlugin({
-        'process.env': {
-            'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
-        }
+    new MiniCssExtractPlugin({
+        filename:'../css/[name].bundle.css'
     })
-];
-
-const resolve = {
-    extensions: [".ts", ".js"]
-}
-
-module.exports = {
-    entry: entry,
-    output: output,
-    resolve: resolve,
-    mode: enviroment,
-    module: _module,
-    optimization: optimization,
-    plugins: plugins
+]
+module.exports={
+    entry,
+    mode,
+    module:_module,
+    output,
+    plugins
 }
